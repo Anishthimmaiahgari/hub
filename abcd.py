ba7f437f8c5c46da1dd3d1c3e30744097e031a5c
Anishthimmaiahgari <anish.thimmaiahgari@bilvantis.io>
2024-11-11 10:50:45 +0530

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -8,4 +8,4 @@ def hello():
     return "Hello, World! Welcome to my Python app running in Docker."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8080)
